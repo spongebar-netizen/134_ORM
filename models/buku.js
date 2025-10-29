e30edee7378d5b6d72a320ae18d202aa3ca73245
Lalu Fatan <lalufatan25@gmail.com>
2025-10-29 12:11:25 +0700

    buku dan komik js

---
+++ models/buku.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Buku extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Buku.init({
+    judul: DataTypes.STRING,
+    penulis: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Buku',
+  });
+  return Buku;
+};
